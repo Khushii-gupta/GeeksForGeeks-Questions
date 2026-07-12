fd618c71a57b3178f063306c34332ffc4370fad8
Khushi Gupta <153846863+Khushii-gupta@users.noreply.github.com>
2026-07-13 00:02:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Floor and Ceil in Unsorted/floor-and-ceil-in-unsorted.cpp
@@ -0,0 +1,45 @@
+class Solution {
+  public:
+    int findFloor(int arr[], int n, int x) {
+        int low = 0, high = n - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] <= x) {
+                ans = arr[mid];     
+                low = mid + 1;      
+            } else {
+                high = mid - 1;     
+            }
+        }
+        return ans;
+    }
+
+    int findCeil(int arr[], int n, int x) {
+        int low = 0, high = n - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] >= x) {
+                ans = arr[mid];     
+                high = mid - 1;     
+            } else {
+                low = mid + 1;     
+            }
+        }
+        return ans;
+    }
+    
+    vector<int> getFloorAndCeil(int x, vector<int> &arr) {
+        // code here
+        sort(arr.begin(), arr.end());
+        int n = arr.size();
+        
+        int floor = findFloor(arr.data(), n, x);
+        int ceil = findCeil(arr.data(), n, x);
+
+        return {floor, ceil};
+    }
+};
